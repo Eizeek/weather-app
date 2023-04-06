aab05a61bf830be64da9a3e0451431697188f1f2
Eizeek <azez.karimov@gmail.com>
2023-04-06 15:55:28 +0400

    Feature/components/weather-data/api-fetch

--- src/components/current-weather/current-weather.js
+++ src/components/current-weather/current-weather.js
@@ -1,18 +1,42 @@
 import "./current-weather.css";
 
-const CurrentWeather = () => {
+const CurrentWeather = ({ data }) => {
   return (
     <div className="weather">
       <div className="top">
         <div>
-        <p className="city">Baku</p>
-        <p className="weather-description">Sunny</p>
+          <p className="city">{data.city}</p>
+          <p className="weather-description">{data.weather[0].description}</p>
         </div>
-        <img alt="weather" className="weather-icon" src="icons/01d.png" />
+        <img alt="weather" className="weather-icon" src={`icons/${data.weather[0].icon}.png`} />
       </div>
       <div className="bottom">
-<p className="temperature">25°</p>
-
+        <p className="temperature">{Math.floor(data.main.temp)}°C</p>
+        <div className="details">
+          <div className="parameter-row">
+            <span className="parameter-label">Details</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Feels Like</span>
+            <span className="parameter-value">{Math.floor(data.main.feels_like)}°</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Wind</span>
+            <span className="parameter-value">{data.wind.speed} m/s</span>
+          </div>
+          <div className="parameter-row">
+            <span Humidity className="parameter-label">
+              Humidity
+            </span>
+            <span className="parameter-value">{data.main.humidity} %</span>
+          </div>
+          <div className="parameter-row">
+            <span Humidity className="parameter-label">
+              Pressure
+            </span>
+            <span className="parameter-value">{data.main.pressure} hPa</span>
+          </div>
+        </div>
       </div>
     </div>
   );
