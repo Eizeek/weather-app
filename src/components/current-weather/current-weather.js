a820451b293fedfe228c660ed1f79c47e082c145
Eizeek <azez.karimov@gmail.com>
2023-04-06 16:32:32 +0400

    Feature/components/forecast

--- src/components/current-weather/current-weather.js
+++ src/components/current-weather/current-weather.js
@@ -8,7 +8,11 @@ const CurrentWeather = ({ data }) => {
           <p className="city">{data.city}</p>
           <p className="weather-description">{data.weather[0].description}</p>
         </div>
-        <img alt="weather" className="weather-icon" src={`icons/${data.weather[0].icon}.png`} />
+        <img
+          alt="weather"
+          className="weather-icon"
+          src={`icons/${data.weather[0].icon}.png`}
+        />
       </div>
       <div className="bottom">
         <p className="temperature">{Math.floor(data.main.temp)}°C</p>
@@ -18,7 +22,9 @@ const CurrentWeather = ({ data }) => {
           </div>
           <div className="parameter-row">
             <span className="parameter-label">Feels Like</span>
-            <span className="parameter-value">{Math.floor(data.main.feels_like)}°</span>
+            <span className="parameter-value">
+              {Math.floor(data.main.feels_like)}°
+            </span>
           </div>
           <div className="parameter-row">
             <span className="parameter-label">Wind</span>
